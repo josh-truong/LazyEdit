3e20670cf7f5e531579923066beb27368a19cdbc
Joshua Truong <joshktruong@gmail.com>
2021-06-28 13:04:13 -0600

    Pass command line arguments

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-cd out/build ; ./LazyEdit
+SRC_FILE="/home/ubuntu/Documents/Github/LazyEdit/resource/hooked.mp4"
+VIDEO_OUTPUT_PATH="/home/ubuntu/Documents/Github/LazyEdit/resource/out/video"
+AUDIO_OUTPUT_PATH="/home/ubuntu/Documents/Github/LazyEdit/resource/out/audio"
+
+cd out/build ; ./LazyEdit $SRC_FILE $VIDEO_OUTPUT_PATH $AUDIO_OUTPUT_PATH
